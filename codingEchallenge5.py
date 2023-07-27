226d2883f58caf273d37a085dc4ac5997226652a
Bernardo Pandolfo <bernardo.pandolfo@longview.com.br>
2023-07-27 16:38:36 -0300

    feat: romantoint

--- codingEchallenge5.py
+++ codingEchallenge5.py
@@ -1,21 +1,9 @@
 def romanToInt(s: str) -> int:
-        cList = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-        iList = []
-        p = 1
-        for key in cList:
-            iList[key] = p
-            if len(iList) % 2 == 0:
-                p = p * 2
-            else:
-                p = p * 5
-        print(iList)
-
-cList = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-iList = []
-p = 1
-for key in cList:
-    iList.append({key:p})
-    if len(iList) % 2 == 0:
-        p = p * 2
-    else:
-        p = p * 5
+        roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        rs = 0
+        for rome in range(len(s)):
+            if rome < len(s)-1 and roman[s[rome]] < roman[s[rome + 1]]:
+                rs -= roman[s[rome]]
+            else: 
+                rs += roman[s[rome]]
+        return rs
